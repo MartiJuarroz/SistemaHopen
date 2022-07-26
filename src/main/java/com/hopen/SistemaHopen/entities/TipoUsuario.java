02a354a47a6075ec9e43a59870afa323d71b1e27
MartiJuarroz <martijuarroz11@gmail.com>
2022-07-26 13:23:56 -0300

    cositass

---
+++ src/main/java/com/hopen/SistemaHopen/entities/TipoUsuario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hopen.SistemaHopen.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Martiniano
+ */
+@Entity
+@Table(name = "tipoUsuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TipoUsuario extends Base{
+    
+    @Column
+    private String nombreTipoUsuario;
+
+}
